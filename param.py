7135d90ac9de78a5e151a572306f8b2f1d67c8a1
unknown <nakshathradeeva@gmail.com>
2025-04-04 14:11:28 +0530

    done

---
+++ param.py
@@ -0,0 +1,12 @@
+
+#Input with paramters
+
+import sys 
+
+print(" Jenkins Build Using Parameters ! ")
+
+X_VALUE = int(sys.argv[1])
+
+Y_VALUE = int(sys.argv[2])
+
+print(" Multiply = ", (X_VALUE * Y_VALUE))
